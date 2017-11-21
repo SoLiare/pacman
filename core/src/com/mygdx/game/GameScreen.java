a0ae62610430cafbe937ba3cea30532ae80ff61f
SoLiare <rawissara.r@ku.th>
2017-11-21 15:27:52 +0700

    Add class World

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -10,17 +10,15 @@ import com.badlogic.gdx.math.Vector2;
 
 public class GameScreen extends ScreenAdapter {
 	private PacmanGame pacmanGame;
-	
 	private Texture pacmanImg;
-	
-	private Pacman pacman;
+	World world;
 	
 	public GameScreen(PacmanGame pacmanGame) {
 		this.pacmanGame = pacmanGame;
 		
 		pacmanImg = new Texture("pacman.png");
 		
-		pacman = new Pacman(100, 100);
+		world = new World(pacmanGame);
 	}
 	
 	@Override
@@ -33,23 +31,24 @@ public class GameScreen extends ScreenAdapter {
 		SpriteBatch batch = pacmanGame.batch;
 		
 		batch.begin();
-		Vector2 pos = pacman.getPosition();
+		Vector2 pos = world.getPacman().getPosition();
 		batch.draw(pacmanImg, pos.x, pos.y);
 		batch.end();
 	}
 	
 	private void update(float delta) {
 		if(Gdx.input.isKeyPressed(Keys.LEFT)) {
-			pacman.move(Pacman.DIRECTION_LEFT);
+			world.getPacman().move(Pacman.DIRECTION_LEFT);
 		}
 		if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
-			pacman.move(Pacman.DIRECTION_RIGHT);
+			world.getPacman().move(Pacman.DIRECTION_RIGHT);
 		}
 		if(Gdx.input.isKeyPressed(Keys.UP)) {
-			pacman.move(Pacman.DIRECTION_UP);
+			world.getPacman().move(Pacman.DIRECTION_UP);
 		}
 		if(Gdx.input.isKeyPressed(Keys.DOWN)) {
-			pacman.move(Pacman.DIRECTION_DOWN);
+			world.getPacman().move(Pacman.DIRECTION_DOWN);
 		}
+		
 	}
 }
--- core/src/com/mygdx/game/Pacman.java
+++ core/src/com/mygdx/game/Pacman.java
@@ -26,9 +26,11 @@ public class Pacman {
     public static final int DIRECTION_DOWN = 3;
     public static final int DIRECTION_LEFT = 4;
     public static final int DIRECTION_STILL = 0;
+    
+    public static final int SPEED = 10;
 	
 	public void move(int dir) {
-		position.x += 10 * DIR_OFFSETS[dir][0];
-		position.y += 10 * DIR_OFFSETS[dir][1];
+		position.x += SPEED * DIR_OFFSETS[dir][0];
+		position.y += SPEED * DIR_OFFSETS[dir][1];
 	}
 }
